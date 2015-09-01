b6b6cfc334e62373d9797cbf6f55b35c89de30b3
Kevin Huelsmann <kjh311@gmail.com>
2015-08-31 18:26:26 -0700

    added the controllers

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,27 @@
-angular.module('beerApp', [])
+angular.module('routerApp', [])
 
 .controller('mainController', function() {
 
   var vm = this;
 
+  vm.bigMessage = 'A smooth sea never made a skilled sailor.';
+})
 
+.controller('homeController', function(){
+
+  var vm = this;
+  vm.message = 'This is the home page!';
+})
+
+.controller('breweriesController', function(){
+
+  var vm = this;
+  vm.message = 'This is the breweries page';
+})
+
+.controller('beerspeakController', function(){
+
+  var vm = this;
+  vm.message = 'This is the Beer Speak Page';
 
 });
