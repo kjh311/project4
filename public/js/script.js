f634b017e04e3108ef312ace9751d6f6b79d3b21
Kevin Huelsmann <kjh311@gmail.com>
2015-11-07 08:51:51 -0800

    beerspeek page edited, auto scroll to top on link clicks

--- public/js/script.js
+++ public/js/script.js
@@ -5,5 +5,18 @@ $( document ).ready(function() {
     RW.render(null, true);
     RW_Async_Init();
     FB.XFBML.parse()
+    $('body').scrollTop(0);
+  });
+
+  $('#searchPageLink').click(function() {
+    $('body').scrollTop(0);
+  });
+
+  $('#breweryPageLink').click(function() {
+    $('body').scrollTop(0);
+  });
+
+  $('#stylesPageLink').click(function() {
+    $('body').scrollTop(0);
   });
 });
